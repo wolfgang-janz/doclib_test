2a3926cd5b3e1f6094233e4297926fcf4f7df790
Wolfgang Janz <wolfgang.janz@exxcellent.de>
2025-07-13 12:08:09 +0200

    test

--- notifyDoctolibDoctorsAppointment.py
+++ notifyDoctolibDoctorsAppointment.py
@@ -3,15 +3,14 @@ import json
 import urllib.parse
 import urllib.request
 
-TELEGRAM_BOT_TOKEN = ''
-TELEGRAM_CHAT_ID = ''
-BOOKING_URL = 'https://www.doctolib.de/'
-AVAILABILITIES_URL = ''
-APPOINTMENT_NAME = None
+TELEGRAM_BOT_TOKEN = '8054097555:AAHWCgOOuNzC6y_D-JlYJZjx1C3H9K83j_g'
+TELEGRAM_CHAT_ID = '-4904641633'
+BOOKING_URL = 'https://www.doctolib.de/hautarzt/blaubeuren/gertraud-kraehn-senftleben/booking/availabilities?specialityId=1289&telehealth=false&placeId=practice-367592&insuranceSectorEnabled=true&insuranceSector=public&isNewPatient=true&isNewPatientBlocked=false&motiveIds%5B%5D=7357714&pid=practice-367592&insurance_sector=public&bookingFunnelSource=profile'
+AVAILABILITIES_URL = 'https://www.doctolib.de/availabilities.json?visit_motive_ids=7357714&agenda_ids=1075438&practice_ids=367592&insurance_sector=public&telehealth=false&start_date=2025-07-13&limit=5'
 MOVE_BOOKING_URL = None
 UPCOMING_DAYS = 15
 MAX_DATETIME_IN_FUTURE = datetime.today() + timedelta(days = UPCOMING_DAYS)
-NOTIFY_HOURLY = False
+NOTIFY_HOURLY = True
 
 if not (
     TELEGRAM_BOT_TOKEN
@@ -20,6 +19,47 @@ if not (
     or AVAILABILITIES_URL
     ) or UPCOMING_DAYS > 15:
     exit()
+
+parsed_url = urllib.parse.urlparse(BOOKING_URL)
+path_parts = parsed_url.path.split('/')
+doctor_name = path_parts[3]
+
+docInfoUrl = f'https://www.doctolib.de/online_booking/api/slot_selection_funnel/v1/info.json?profile_slug={doctor_name}'
+docInfoRequest = urllib.request.Request(docInfoUrl)
+docInfoRequest.add_header(
+    'User-Agent',
+    'Mozilla/5.0 (Macintosh; Intel Mac OS X 13_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36'
+)
+
+docInfo = (urllib.request
+           .urlopen(docInfoRequest)
+           .read()
+           .decode('utf-8'))
+
+docInfoJson = json.loads(docInfo)
+
+# Extract the doctor's name with title
+doctor_name_with_title = None
+first_agenda_id = None
+if isinstance(docInfoJson, dict) and 'data' in docInfoJson:
+    data = docInfoJson['data']
+    if isinstance(data, dict) and 'profile' in data:
+        profile = data['profile']
+        if isinstance(profile, dict):
+            doctor_name_with_title = (profile.get('name_with_title') or 
+                                    profile.get('full_name') or 
+                                    profile.get('name'))
+    
+    # Extract the first agenda ID
+    if isinstance(data, dict) and 'agendas' in data:
+        agendas = data['agendas']
+        if isinstance(agendas, list) and len(agendas) > 0:
+            first_agenda = agendas[0]
+            if isinstance(first_agenda, dict) and 'id' in first_agenda:
+                first_agenda_id = first_agenda['id']
+
+if not doctor_name_with_title:
+    doctor_name_with_title = doctor_name
 
 urlParts = urllib.parse.urlparse(AVAILABILITIES_URL)
 query = dict(urllib.parse.parse_qsl(urlParts.query))
@@ -38,15 +78,22 @@ request.add_header(
     'Mozilla/5.0 (Macintosh; Intel Mac OS X 13_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36'
 )
 response = (urllib.request
-                    .urlopen(request)
-                    .read()
-                    .decode('utf-8'))
+            .urlopen(request)
+            .read()
+            .decode('utf-8'))
+
+
+
+
+
+
+
+
 
 availabilities = json.loads(response)
 
 slotsInNearFuture = availabilities['total']
 slotInNearFutureExist = slotsInNearFuture > 0
-earlierSlotExists = False
 if slotInNearFutureExist:
     for day in availabilities['availabilities']:
         if len(day['slots']) == 0:
@@ -55,40 +102,35 @@ if slotInNearFutureExist:
         nextDatetime = (datetime.fromisoformat(nextDatetimeIso8601)
                                 .replace(tzinfo = None))
         if nextDatetime < MAX_DATETIME_IN_FUTURE:
-            earlierSlotExists = True
             break;
 
-isOnTheHour = datetime.now().minute == 0
-isHourlyNotificationDue = isOnTheHour and NOTIFY_HOURLY
-
-if not (earlierSlotExists or isHourlyNotificationDue):
-    exit()
 
 message = ''
-if APPOINTMENT_NAME:
-    message += f'👨‍⚕️👩‍⚕️ {APPOINTMENT_NAME}'
-    message += '\n'
 
-if earlierSlotExists:
+if slotInNearFutureExist:
     pluralSuffix = 's' if slotsInNearFuture > 1 else ''
-    message += f'🔥 {slotsInNearFuture} slot{pluralSuffix} within {UPCOMING_DAYS}d!'
+    message += f'🔥 {slotsInNearFuture} slot{pluralSuffix} within {UPCOMING_DAYS}d! for {doctor_name_with_title}'
     message += '\n'
     if MOVE_BOOKING_URL:
         message += f'<a href="{MOVE_BOOKING_URL}">🚚 Move existing booking</a>.'
         message += '\n'
 
-if isHourlyNotificationDue:
+else:
     nextSlotDatetimeIso8601 = availabilities['next_slot']
     nextSlotDate = (datetime.fromisoformat(nextSlotDatetimeIso8601)
                                 .strftime('%d %B %Y'))
-    message += f'🐌 slot <i>{nextSlotDate}</i>.'
+    message += f'🐌 slot <i>{nextSlotDate}</i> for <i>{doctor_name_with_title}</i>.'
     message += '\n'
 
 message += f'Book now on <a href="{BOOKING_URL}">doctolib.de</a>.'
 
+print(message)
+"""
 urlEncodedMessage = (urllib
                         .parse
                         .quote(message))
+
+
 (urllib
     .request
     .urlopen(
@@ -98,3 +140,4 @@ urlEncodedMessage = (urllib
         f'&parse_mode=HTML'
         f'&disable_web_page_preview=true')
     ))
+"""
